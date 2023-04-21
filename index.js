fac7c0481c069e550f8836be3330bc3274204d4b
FabianOliva <fabianoliva01@hotmail.com>
2023-04-21 19:15:32 -0300

    Fabi 21/4

--- index.js
+++ index.js
@@ -24,41 +24,38 @@ burgerIcon.addEventListener("click", toggleMenu);
 //Modal
 let btnOpenModal = document.getElementById("btnopenModal");
 let modal = document.getElementById("modal");
-let btnCloseModal = document.getElementById('btnCloseModal');
-let btnOpenModalByClass = document.getElementsByClassName("btnOpenClass")[0];//aqui llamo al primer elemento que cumple con esa clase
-let btnOpenModalByClassBurger = document.getElementsByClassName("btnOpenClass")[1];//aqui llamo al segundo
+let btnCloseModal = document.getElementById("btnCloseModal");
+let btnOpenModalByClass = document.getElementsByClassName("btnOpenClass")[0]; //aqui llamo al primer elemento que cumple con esa clase
+let btnOpenModalByClassBurger = document.getElementsByClassName("btnOpenClass")[1]; //aqui llamo al segundo
 //quisimos evitar usar el mismo id en dos elementos por eso creamos la clase btnOpenClass para poder tarjetear dos elementos
 let isModalOpen = false;
 let containerGeneral = document.getElementById("containerGeneral");
-let animatedDiv = document.getElementById("animatedDiv")
+let animatedDiv = document.getElementsByClassName("animatedDiv")[0];
 
-btnOpenModalByClass.addEventListener("click", function (){
-    console.log('abriendo modal');
-    modal.style.display = "block";
-    btnOpenModal.style.display = "none";
-    document.querySelector(".modal-content").classList.add("animate__fadeInDown");    
+btnOpenModalByClass.addEventListener("click", function () {
+  console.log("abriendo modal");
+  modal.style.display = "block";
+  btnOpenModal.style.display = "none";
+  animatedDiv.classList.add("animate__fadeInDown");
 });
-btnOpenModalByClassBurger.addEventListener("click", function (){
+btnOpenModalByClassBurger.addEventListener("click", function () {
   modal.style.display = "flex";
   btnOpenModal.style.display = "none";
   containerGeneral.style.display = "none";
   animatedDiv.classList.remove("animate__animated");
-
 });
 btnCloseModal.addEventListener("click", function () {
-    console.log("cerrando modal");    
-        modal.style.display = "none";
-        btnOpenModal.style.display = "block";
-        containerGeneral.style.display = "block"
-
+  console.log("cerrando modal");
+  modal.style.display = "none";
+  btnOpenModal.style.display = "block";
+  containerGeneral.style.display = "block";
 });
 //esta parte de window.onclick no anda no se porque...
-window.addEventListener("click", function (){
+window.addEventListener("click", function () {
   if (target != modal) {
     modal.style.display = "none";
-}
-})
-
+  }
+});
 
 // window.onclick = function(event) {
 //     if (event.target == modal) {
--- modalModel.js
+++ modalModel.js
@@ -1,23 +1,22 @@
 var btnOpenModal = document.getElementById("btnopenModal");
 var modal = document.getElementById("modal");
-var btnCloseModal = document.getElementById('btnCloseModal');
-
+var btnCloseModal = document.getElementById("btnCloseModal");
 
 btnOpenModal.onclick = function () {
-    modal.style.display = "block";
-    btnOpenModal.style.display = "none";
-    modal.querySelector(".modal-content").classList.add("animate__fadeInDown");
-}
+  modal.style.display = "block";
+  btnOpenModal.style.display = "none";
+  modal.querySelector(".modal-content").classList.add("animate__fadeInDown");
+};
 
 btnCloseModal.addEventListener("click", function () {
-    modal.querySelector(".modal-content").classList.add("animate__fadeOutDown");
-    modal.querySelector(".modal-content").addEventListener("animationend", function () {
-        modal.style.display = "none";
-    });
+  modal.querySelector(".modal-content").classList.add("animate__fadeOutDown");
+  modal.querySelector(".modal-content").addEventListener("animationend", function () {
+    modal.style.display = "none";
+  });
 });
 //esta parte de window.onclick no anda no se porque...
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
